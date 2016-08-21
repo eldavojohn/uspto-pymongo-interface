9555ffeb5fb590cee55c403184fa632aa7bf5c88
eldavojohn <eldavojohn@gmail.com>
2016-08-21 18:03:44 -0400

    Initial queries to the database demonstrated.

--- tests/test_advanced.py
+++ tests/test_advanced.py
@@ -1,12 +1,13 @@
- .context import uspto
+from .context import uspto
 
 import unittest
 
-
 class AdvancedTestSuite(unittest.TestCase):
     """Advanced test cases."""
+    print "here we go"
 
-    def test_thoughts(self):
+
+    def test_patent_crawl(self):
         uspto.crawl_patents()
 
 
--- uspto/uspto.py
+++ uspto/uspto.py
@@ -1,10 +1,10 @@
 import pymongo
 from pymongo import MongoClient
 
-
-def start_client():
-    client = MongoClient('localhost', 27017)
-    return 'started...'
-
 def crawl_patents():
-    print start_client()
+    client = MongoClient('localhost', 27017)
+    db = client.uspto
+    patent_collection = db.patents
+    first_patent = patent_collection.find_one()
+    print first_patent
+    
